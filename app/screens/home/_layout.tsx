eb194c40bd95ebdc5566fabbefd4b0e714018d1b
NightmareFox <carloshenriquez392004@gmail.com>
2025-05-07 20:01:18 -0400

    Refactor HomeLayout component for improved code readability and organization
    - Cleaned up Icon component formatting for better alignment and consistency.
    - Updated drawer screen options for 'Crear Cliente' and 'Crear Producto' with appropriate icons and labels.

--- app/screens/home/_layout.tsx
+++ app/screens/home/_layout.tsx
@@ -12,9 +12,7 @@ export default function HomeLayout() {
             drawerLabel: 'Inicio',
             title: 'Inicio',
             drawerIcon: (props) => (
-              <Icon source={'home'} 
-              size={props.size} 
-              color={props.color} />
+              <Icon source={'home'} size={props.size} color={props.color} />
             ),
           }}
         />
@@ -40,10 +38,11 @@ export default function HomeLayout() {
             drawerLabel: 'Clientes',
             title: 'Clientes',
             drawerIcon: (props) => (
-              <Icon 
-              source={'account-group'} 
-              size={props.size} 
-              color={props.color} />
+              <Icon
+                source={'account-group'}
+                size={props.size}
+                color={props.color}
+              />
             ),
           }}
         />
@@ -54,24 +53,37 @@ export default function HomeLayout() {
             drawerLabel: 'Historial',
             title: 'Historial',
             drawerIcon: (props) => (
-              <Icon 
-              source={'history'} 
-              size={props.size} 
-              color={props.color} />
+              <Icon source={'history'} size={props.size} color={props.color} />
             ),
           }}
         />
 
         <Drawer.Screen
-          name='settings/index'
+          name='clients/form'
           options={{
-            drawerLabel: 'Configuración',
-            title: 'Configuración',
+            drawerLabel: 'Crear Cliente',
+            title: 'Crear Cliente',
             drawerIcon: (props) => (
-              <Icon 
-              source={'cog'} 
-              size={props.size} 
-              color={props.color} />
+              <Icon
+                source={'account-plus'}
+                size={props.size}
+                color={props.color}
+              />
+            ),
+          }}
+        />
+
+        <Drawer.Screen
+          name='products/form'
+          options={{
+            drawerLabel: 'Crear Producto',
+            title: 'Crear Producto',
+            drawerIcon: (props) => (
+              <Icon
+                source={'plus-box'}
+                size={props.size}
+                color={props.color}
+              />
             ),
           }}
         />
