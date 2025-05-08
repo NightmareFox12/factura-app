55b01c8f9ce0fb4d485b79eb60f142629d1eb744
NightmareFox <carloshenriquez392004@gmail.com>
2025-05-08 11:08:11 -0400

    Refactor printing functionality and introduce invoice template
    - Replaced hardcoded HTML in the printing functions with a new invoice template for better maintainability.
    - Updated button styles and text for improved user experience in the Testing screen.
    - Removed unused printer selection functionality to streamline the code.

--- app/screens/home/testing/index.tsx
+++ app/screens/home/testing/index.tsx
@@ -1,59 +1,44 @@
-import { useState } from 'react';
-import { View, StyleSheet, Button, Platform, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import * as Print from 'expo-print';
 import { shareAsync } from 'expo-sharing';
-
-const html = `
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
-  </head>
-  <body style="text-align: center;">
-    <h1 style="font-size: 50px; font-family: Helvetica Neue; font-weight: normal;">
-      Hello Expo!
-    </h1>
-    <img
-      src="https://d30j33t1r58ioz.cloudfront.net/static/guides/sdk.png"
-      style="width: 90vw;" />
-  </body>
-</html>
-`;
+import { invoiceTemplate } from '@/templates/invoiceTemplate';
+import { Button } from 'react-native-paper';
 
 export default function App() {
   // const [selectedPrinter, setSelectedPrinter] = useState();
 
   const print = async () => {
-    // On iOS/android prints the given html. On web prints the HTML from the current page.
     await Print.printAsync({
-      html,
-      // printerUrl: selectedPrinter?.url, // iOS only
+      html: invoiceTemplate,
     });
   };
 
   const printToFile = async () => {
-    // On iOS/android prints the given html. On web prints the HTML from the current page.
-    const { uri } = await Print.printToFileAsync({ html });
+    const { uri } = await Print.printToFileAsync({ html: invoiceTemplate });
     console.log('File has been saved to:', uri);
     await shareAsync(uri, { UTI: '.pdf', mimeType: 'application/pdf' });
   };
 
-  const selectPrinter = async () => {
-    const printer = await Print.selectPrinterAsync(); // iOS only
-    // setSelectedPrinter(printer);
-  };
+  // const selectPrinter = async () => {
+  // const printer = await Print.selectPrinterAsync();
+  // setSelectedPrinter(printer);
+  // };
 
   return (
     <View style={styles.container}>
-      <Button title='Print' onPress={print} />
-      <View style={styles.spacer} />
-      <Button title='Print to PDF file' onPress={printToFile} />
-      {Platform.OS === 'ios' && (
-        <>
-          <View style={styles.spacer} />
-          <Button title='Select printer' onPress={selectPrinter} />
-          <View style={styles.spacer} />
-        </>
-      )}
+      <Button mode='contained' style={styles.button} onPress={print}>
+        Descargar PDF
+      </Button>
+
+      <Button mode='contained' style={styles.button} onPress={printToFile}>
+        Compartir PDF
+      </Button>
+
+      {/* {Platform.OS === 'ios' && (
+        <Button onPress={print} onPress={selectPrinter}>
+          Seleccionar impresion{' '}
+        </Button>
+      )} */}
     </View>
   );
 }
@@ -62,14 +47,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    flexDirection: 'column',
-    padding: 8,
+    gap: 10,
   },
-  spacer: {
-    height: 8,
-  },
-  printer: {
-    textAlign: 'center',
+  button: {
+    marginHorizontal: 10,
   },
 });
---
+++ templates/invoiceTemplate.ts
@@ -0,0 +1,15 @@
+export const invoiceTemplate = `
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+  </head>
+  <body style="text-align: center;">
+    <h1 style="font-size: 50px; font-family: Helvetica Neue; font-weight: normal;">
+      Documento Fiscal
+    </h1>
+    <img
+      src="https://elceo.com/wp-content/uploads/2023/07/CSF-1.jpg"
+      style="width: 90vw;" />
+  </body>
+</html>
+`;
